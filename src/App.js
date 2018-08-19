540062b6617f5f0fd39d26d15319f6bd74672290
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-08-19 17:27:48 -0400

    Main layout fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBuild from './containers/BurgerBuild/BurgerBuild';
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <Layout>
+          <BurgerBuild />
+        </Layout>
       </div>
     );
   }
