d221f117ca29c383b11b78939557915a6c29ca48
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-01 16:28:05 -0400

    Ingreients component added

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
+];
 
 const buildControls = (props) => {
-    return (
-        <div>
 
+
+
+    return (
+        <div className={ classes.BuildControls }>
+            { controls.map(ctrl => {
+                return <BuildControl added={ () => props.ingredientAdded(ctrl.type) } key={ ctrl.label } label={ ctrl.label } />
+            }) }
         </div>
     )
 };
