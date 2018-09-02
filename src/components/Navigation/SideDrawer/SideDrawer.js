0a2f7616411ea1477af9babfc41627eaac861e8b
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-02 15:34:49 -0400

    Order window feature expanded

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,12 +6,17 @@ import classes from "./SideDrawer.css";
 import Backdrop from "../../UI/Backdrop/Backdrop";
 import Aux from "../../../hoc/Aux";
 
-const sideDrawer =() => {
+const sideDrawer =(props) => {
+    let attachedClasses = [ classes.SideDrawer, classes.Close ];
 
+    if(props.open) {
+        attachedClasses = [ classes.SideDrawer, classes.Open ];
+
+    }
     return (
         <Aux>
-        <Backdrop show/>
-        <div className={ classes.SideDrawer }>
+        <Backdrop show={ props.open } clicked={ props.closed }/>
+        <div className={ attachedClasses.join(" ") }>
             <Logo height="11%"/>
             <nav>
                 <NavigationItems />
