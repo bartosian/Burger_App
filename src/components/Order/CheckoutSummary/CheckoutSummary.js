60eee312e6e944db7d3214b8c358a9176bd84f93
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-09 16:22:04 -0400

    Added summary structure

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const checkoutSummary = (props) => {
+    return (
+        <div>
+            <h1>We hope it tastes well!</h1>
+        </div>
+    );
+}
+
+export default checkoutSummary;
+
