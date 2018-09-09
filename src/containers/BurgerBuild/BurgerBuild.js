33fde829fa1d00ca7f30a757b3a9a03492e8d063
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-09 17:07:22 -0400

    Added withRouter

--- src/containers/BurgerBuild/BurgerBuild.js
+++ src/containers/BurgerBuild/BurgerBuild.js
@@ -105,34 +105,34 @@ class BurgerBuild extends Component {
 
     purchaseContinuehandler = () => {
 
-        this.setState({
-            loading: true
-        });
-        const order = {
-          ingredients: this.state.ingredients,
-          price: this.state.totalPrice,
-          customer: {
-              name: "Kiryl",
-              age: 28,
-              city: "Miami"
-          }
-        };
-
-        // alert("You continue!");
-        Axios.post("/orders.json", order)
-            .then(response => {
-                this.setState({
-                    loading: false,
-                    purchasing: false
-                })
-            })
-            .catch(err => {
-                this.setState({
-                    loading: false,
-                    purchasing: false
-                })
-            });
-
+        // this.setState({
+        //     loading: true
+        // });
+        // const order = {
+        //   ingredients: this.state.ingredients,
+        //   price: this.state.totalPrice,
+        //   customer: {
+        //       name: "Kiryl",
+        //       age: 28,
+        //       city: "Miami"
+        //   }
+        // };
+        //
+        // // alert("You continue!");
+        // Axios.post("/orders.json", order)
+        //     .then(response => {
+        //         this.setState({
+        //             loading: false,
+        //             purchasing: false
+        //         })
+        //     })
+        //     .catch(err => {
+        //         this.setState({
+        //             loading: false,
+        //             purchasing: false
+        //         })
+        //     });
+        this.props.history.push("/checkout");
     }
 
     render() {
