e407924ca4d3c2cdef71a45d645fae3286da5ed1
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-22 14:12:01 -0400

    Created select component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -8,8 +8,14 @@ const input = (props) => {
         case ('input'):
             inputElement = <input className={ classes.InputElement } { ...props.elementConfig } value={ props.value }/>;
             break;
-        case ('textarea'):
-            inputElement = <textarea className={ classes.InputElement } { ...props.elementConfig } value={ props.value }/>;
+        case ('select'):
+            inputElement = (
+                <select className={ classes.InputElement }  value={ props.value }>
+                    { props.elementConfig.options.map( option => (
+                        <option value={ option.value } key={ option.value }>{ option.displayValue }</option>
+                    ))}
+                </select>
+            );
             break;
         default:
             inputElement = <input className={ classes.InputElement } { ...props.elementConfig } value={ props.value }/>;
