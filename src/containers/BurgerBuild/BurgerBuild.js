4da1504ae3cc3d092c5de2e994b6746e84a72936
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-23 12:40:01 -0400

    Changed reducer

--- src/containers/BurgerBuild/BurgerBuild.js
+++ src/containers/BurgerBuild/BurgerBuild.js
@@ -11,12 +11,6 @@ import *  as actions from '../../store/actions';
 import { connect } from 'react-redux';
 
 
-const INGREDIENT_PRICES = {
-  salad: 0.5,
-  cheese: 0.4,
-  meat: 1.3,
-  bacon: 0.7
-};
 
 class BurgerBuild extends Component {
 
